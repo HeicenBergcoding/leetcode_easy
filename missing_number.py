2679a57a78e22086df916f0a65a89d37e059889a
heisenberg131 <abhilash@microsec.ai>
2026-01-23 03:05:35 +0530

    hell nah

--- missing_number.py
+++ missing_number.py
@@ -7,3 +7,5 @@ class Solution:
 
 # we calculate the actual sum of the array using sum of naural numbers
 # subtract it with actual sum to get the missing number 
+# o(n) time complexity as sum fucntion iterate over the nums
+# o(1) because we use only constant extra variables
---
+++ pivot_index.py
@@ -0,0 +1,13 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        array_sum = sum(nums)
+        left_sum = 0
+        for i in range(len(nums)):
+            right_sum = array_sum - left_sum - nums[i]
+            if left_sum == right_sum:
+                return i
+            left_sum+=nums[i]
+        return -1
+
+# calculate total sum
+# keep track of left total and right total
